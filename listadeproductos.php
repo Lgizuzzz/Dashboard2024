bfea1507b42bca02fabf254faa435664a38f25e3
Lgizuzzz <frankerguz123@gmail.com>
2024-06-17 22:31:36 -0500

    first commit

---
+++ listadeproductos.php
@@ -0,0 +1,104 @@
+<?php require_once "vistas/parte_superior.php"?>
+
+<!--INICIO del cont principal-->
+<div class="container">
+    <h1>Lista de Productos</h1>
+
+<?php
+include_once 'bd/conexion.php';
+$objeto = new Conexion();
+$conexion = $objeto->Conectar();
+
+$consulta = "SELECT id, cantidad, descripcion, nombre, precio FROM productos";
+$resultado = $conexion->prepare($consulta);
+$resultado->execute();
+$data=$resultado->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+
+<div class="container">
+        <div class="row">
+            <div class="col-lg-12">            
+            <button id="btnNuevo" type="button" class="btn btn-success" data-toggle="modal">Nuevo</button>    
+            </div>    
+        </div>    
+    </div>    
+    <br>  
+<div class="container">
+        <div class="row">
+                <div class="col-lg-12">
+                    <div class="table-responsive">        
+                        <table id="tablaProductos" class="table table-striped table-bordered table-condensed" style="width:100%">
+                        <thead class="text-center">
+                            <tr>
+                                <th>Id</th>
+                                <th>Nombre</th>
+                                <th>Descripcion</th>                                
+                                <th>Cantidad</th>  
+                                <th>Precio</th>  
+                                <th>Acciones</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php                            
+                            foreach($data as $dat) {                                                        
+                            ?>
+                            <tr>
+                                <td><?php echo $dat['id'] ?></td>
+                                <td><?php echo $dat['nombre'] ?></td>
+                                <td><?php echo $dat['descripcion'] ?></td>
+                                <td><?php echo $dat['cantidad'] ?></td>    
+                                <td><?php echo $dat['precio'] ?></td>    
+                                <td></td>
+                            </tr>
+                            <?php
+                                }
+                            ?>                                
+                        </tbody>        
+                       </table>                    
+                    </div>
+                </div>
+        </div>  
+    </div>    
+      
+<!--Modal para CRUD-->
+<div class="modal fade" id="modalCRUD" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel"></h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+        <form id="formProductos">    
+            <div class="modal-body">
+                <div class="form-group">
+                <label for="nombre" class="col-form-label">Nombre:</label>
+                <input type="text" class="form-control" id="nombre">
+                </div> 
+                <div class="form-group">
+                <label for="descripcion" class="col-form-label">Descripcion:</label>
+                <input type="text" class="form-control" id="descripcion">
+                </div>                
+                <div class="form-group">
+                <label for="cantidad" class="col-form-label">Cantidad:</label>
+                <input type="number" class="form-control" id="cantidad">
+                </div>           
+                <div class="form-group">
+                <label for="precio" class="col-form-label">Precio:</label>
+                <input type="number" class="form-control" id="precio">
+                </div>            
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-light" data-dismiss="modal">Cancelar</button>
+                <button type="submit" id="btnGuardar" class="btn btn-dark">Guardar</button>
+            </div>
+        </form>    
+        </div>
+    </div>
+</div>  
+    
+</div>
+<!--FIN del cont principal-->
+
+<?php require_once "vistas/parte_inferior.php"?>
